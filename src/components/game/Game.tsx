ed80a2d3ace447c9a6f092b70d04909b425b3079
Alireza <alireza.es@gmail.com>
2021-12-02 22:29:31 +0330

    Create borad stetch

---
+++ src/components/game/Game.tsx
@@ -0,0 +1,17 @@
+import Board from "../board/Board";
+
+function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board height={100} />
+      </div>
+      <div className="game-info">
+        <div>{/* status */}</div>
+        <ol>{/* TODO */}</ol>
+      </div>
+    </div>
+  );
+}
+
+export default Game;
---
+++ src/components/square/Square.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import SquareProps from "./SquareProps";
+import "./SquareStyle.scss";
+
+function Square(props: SquareProps & React.HTMLAttributes<HTMLDivElement>) {
+  const [state, setState] = React.useState<SquareProps>(props);
+  
+  function squareClicked() {
+    setState({value: 'X'});
+  }
+  return (
+    <button className="square" onClick={squareClicked}>{state.value}</button>
+  );
+}
+
+export default Square;
